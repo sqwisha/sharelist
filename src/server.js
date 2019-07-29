731b98c29b5b65354b81fe281438a6a1513d3be5
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-07-29 10:53:24 -0700

    add static and other paths

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 5000;
 const bodyParser = require('body-parser');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 require('./routes')(app);
-
-app.get('/', (req, res) => {
-  res.send('PORT 5000');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
