ddbe34ceda31a2479b1f2eadbd9d2ca117aaab42
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-07-29 12:02:17 -0700

    change server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 5000;
-const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 require('./routes')(app);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join((__dirname = 'client/build/index.html')));
+  });
+} else {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/public/index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
