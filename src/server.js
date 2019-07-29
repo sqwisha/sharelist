e78b06de8be1a881cdc0156f6076aee6f94cf83e
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-07-29 12:15:44 -0700

    fix path in server.js

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ require('./routes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
-    res.sendFile(path.join((__dirname, 'client/build/index.html')));
+    res.sendFile(path.join((__dirname, '/client/build/index.html')));
   });
 } else {
   app.get('*', (req, res) => {
